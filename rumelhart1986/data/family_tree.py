5c29d1303a5b9ec7d99884df7efb75802be4da27
Yaseen-D-Shaik <yaseenshaikbmw@gmail.com>
2026-07-24 10:24:47 +0530

    Add family tree data and relationships for Rumelhart 1986 reproduction

--- rumelhart1986/data/family_tree.py
+++ rumelhart1986/data/family_tree.py
@@ -1,3 +1,14 @@
 """Family tree triples for the Rumelhart 1986 reproduction."""
 
 # Add family tree data here.
+PEOPLE= ["Christopher","Roberto","Penelope", "Maria", "Andrew", "pierro",
+         "christine","francesca","arthur","emilio","margaret","gina","james",
+         "Marco","Victoria","lucia","charies","tomaso","jennifer",
+         "Angela","colin","Alfonso","charlotte","sophia"]
+
+
+RELATIONSHIPS= ["father", "mother","husband", "wife", "son", "daughter", 
+                "uncle", "aunt", "brother", "sister", "nephew","niece"]
+
+
+TRIPLES= {}
