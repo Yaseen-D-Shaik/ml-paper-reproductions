8978261351adadae21f33f1e83b7d8f5c9c8a967
Yaseen-D-Shaik <yaseenshaikbmw@gmail.com>
2026-08-24 22:57:30 +0530

    Update .gitignore to include FILE_REFERENCE.md and remove unused _seed_argmax_check.py script

--- _seed_argmax_check.py
+++
@@ -1,8 +0,0 @@
-import train
-
-for seed in [1, 7, 42, 123]:
-    train.SEED = seed
-    m = train.train()
-    tc, tt = train.evaluate_argmax(m, train.TEST_TRIPLES, label="TEST", verbose=False)
-    vc, vt = train.evaluate_argmax(m, train.VAL_TRIPLES, label="VAL", verbose=False)
-    print(f"seed={seed}: TEST_TRIPLES {tc}/{tt}, VAL_TRIPLES {vc}/{vt}", flush=True)
